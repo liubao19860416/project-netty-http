5e461348bcda858844703ebcf2237a6e3f9819c6
18611478781 <18611478781@163.com>
2016-04-15 18:02:25 +0800

    升级redis版本

--- src/main/java/com/saic/ebiz/vbox/utils/RedisClient.java
+++ src/main/java/com/saic/ebiz/vbox/utils/RedisClient.java
@@ -1,9 +1,14 @@
 package com.saic.ebiz.vbox.utils;
 
+import static org.junit.Assert.*;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import junit.framework.Assert;
+
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,22 +17,73 @@ import redis.clients.jedis.JedisShardInfo;
 import redis.clients.jedis.ShardedJedis;
 import redis.clients.jedis.ShardedJedisPool;
 
-import com.saic.ebiz.vbox.conf.CustomizedPropertyPlaceholderConfigurer;
-
 /**
  * Redis客户端
  * 
  * @author Liubao
- * @version 3.0
- * @Date 2015年11月16日
+ * @version redis.version==>2.7.2
+ * @Date 2016年04月15日
  * 
  */
 public class RedisClient {
     private static Logger logger = LoggerFactory.getLogger(RedisClient.class);
 
+    //初始化常量信息,dbIndex1从0开始
+    private static final int dbIndex1=1;
+    private static final String host1="192.168.2.182";
+    private static final int port1=6379;
+    private static final String auth1="jh123";
+    private static final String host2="192.168.2.192";
+    private static final int port2=6379;
+    private static final int dbIndex2=2;
+    private static final String auth2="jh123";
+    //格式
+//    private static final String uri1="redis://:jh123@192.168.2.182:6379/0";
+//    private static final String uri2="redis://:jh123@192.168.2.192:6379/2";
+    private static final String uri1;
+    private static final String uri2;
+    
     private static ShardedJedisPool sjp = null;
-
+    
+    /**
+     * 测试dbIndex方法
+     */
+    @Test
+    public void testName0() throws Exception {
+        String value = get("loginKey_5");
+        System.out.println(value);
+        value = get("loginKey_10");
+        System.out.println(value);
+    }
+    
+    @Test
+    public void testName1() throws Exception {
+        String key="userName";
+        String value="LiuBao";
+        String result = put(key, value);
+        System.out.println(result);
+        key="userName1";
+        value="LiuBao1";
+        result = put(key, value);
+        System.out.println(result);
+        value = get(key);
+        System.out.println(value);
+    }
+    
+    @Test
+    public void testName2() throws Exception {
+        String key="userName";
+        String value = get(key);
+        System.out.println(value);
+    }
+    
     static {
+        uri1="redis://:"+auth1+"@"+host1+":"+port1+"/"+dbIndex1;
+        uri2="redis://:"+auth2+"@"+host2+":"+port2+"/"+dbIndex2;
+        logger.warn("初始化uri信息为:uri1={}",uri1);
+        logger.warn("初始化uri信息为:uri2={}",uri2);
+        Assert.assertNotNull(uri1);
+        assertNotNull(uri2);
         init();
     }
 
@@ -36,15 +92,27 @@ public class RedisClient {
      */
     protected static synchronized boolean init() {
         List<JedisShardInfo> shards = new ArrayList<JedisShardInfo>();
-        shards.indexOf(CustomizedPropertyPlaceholderConfigurer.getContextProperty("redis.cxbservice.index"));
-        shards.add(new JedisShardInfo(CustomizedPropertyPlaceholderConfigurer.getContextProperty("redis.cxbservice.host1"), 
-                Integer.parseInt(CustomizedPropertyPlaceholderConfigurer.getContextProperty("redis.cxbservice.port1"))));
-        shards.add(new JedisShardInfo(CustomizedPropertyPlaceholderConfigurer.getContextProperty("redis.cxbservice.host2"), 
-                Integer.parseInt(CustomizedPropertyPlaceholderConfigurer.getContextProperty("redis.cxbservice.port2"))));
-        logger.warn("从ucm中加载要redis1配置信息，值为："
-                + CustomizedPropertyPlaceholderConfigurer.getContextProperty("redis.cxbservice.host1")
-                + "从ucm中加载要redis2配置信息，值为："
-                + CustomizedPropertyPlaceholderConfigurer.getContextProperty("redis.cxbservice.host2"));
+        /**
+         * 必须使用如下方式初始化,才能构建对应的dbIndex信息
+         */
+        JedisShardInfo jedisShardInfo1 = new JedisShardInfo(uri1);
+        JedisShardInfo jedisShardInfo2 = new JedisShardInfo(uri2);
+        shards.add(jedisShardInfo1);
+        shards.add(jedisShardInfo2);
+        sjp = new ShardedJedisPool(new JedisPoolConfig(), shards);
+        return true;
+    }
+    
+    @Deprecated
+    protected static synchronized boolean init0() {
+        List<JedisShardInfo> shards = new ArrayList<JedisShardInfo>();
+        //shards.indexOf(dbIndex);
+        JedisShardInfo jedisShardInfo1 = new JedisShardInfo(host1, port1);
+        jedisShardInfo1.setPassword(auth1);
+        JedisShardInfo jedisShardInfo2 = new JedisShardInfo(host2, port2);
+        jedisShardInfo2.setPassword(auth2);
+        shards.add(jedisShardInfo1);
+        shards.add(jedisShardInfo2);
         sjp = new ShardedJedisPool(new JedisPoolConfig(), shards);
         return true;
     }
@@ -54,49 +122,53 @@ public class RedisClient {
      * @return Jedis
      * @throws
      */
-    public static ShardedJedis getRedis() {
+    private static ShardedJedis getRedis() {
         logger.info("RedisManage.getRedis  start");
         return sjp.getResource();
     }
 
-    public String put(String key, String value) {
+    public static String put(String key, String value) {
         ShardedJedis jedis = getRedis();
         try {
             jedis.set(key, value);
             return key;
         } finally {
-            sjp.returnResource(jedis);
+            //sjp.returnResource(jedis);
+            sjp.returnResourceObject(jedis);
         }
     }
 
-    public String put(String key, Map<String, String> hash) {
+    public static String put(String key, Map<String, String> hash) {
         ShardedJedis jedis = getRedis();
         try {
             jedis.hmset(key, hash);
             return key;
         } finally {
-            sjp.returnResource(jedis);
+            //sjp.returnResource(jedis);
+            sjp.returnResourceObject(jedis);
         }
     }
 
-    public String put(String key, String value, int Second) {
+    public static String put(String key, String value, int Second) {
         ShardedJedis jedis = getRedis();
         try {
             jedis.set(key, value);
             jedis.expire(key, Second);
             return key;
         } finally {
-            sjp.returnResource(jedis);
+            //sjp.returnResource(jedis);
+            sjp.returnResourceObject(jedis);
         }
     }
 
-    public String delete(String key) {
+    public static String delete(String key) {
         ShardedJedis jedis = getRedis();
         try {
             jedis.del(key);
             return key;
         } finally {
-            sjp.returnResource(jedis);
+            //sjp.returnResource(jedis);
+            sjp.returnResourceObject(jedis);
         }
     }
 
@@ -104,44 +176,48 @@ public class RedisClient {
     // getJedis().flushAll();
     // }
 
-    public void lpus(String key, String... value) {
+    public static void lpus(String key, String... value) {
         ShardedJedis jedis = getRedis();
         try {
             jedis.lpush(key, value);
         } finally {
-            sjp.returnResource(jedis);
+            //sjp.returnResource(jedis);
+            sjp.returnResourceObject(jedis);
         }
     }
 
-    public Map<String, String> getMap(String key) {
+    public static Map<String, String> getMap(String key) {
         ShardedJedis jedis = getRedis();
         try {
             return jedis.hgetAll(key);
         } finally {
-            sjp.returnResource(jedis);
+            //sjp.returnResource(jedis);
+            sjp.returnResourceObject(jedis);
         }
     }
 
-    public List<String> getList(String key) {
+    public static List<String> getList(String key) {
         ShardedJedis jedis = getRedis();
         try {
             return jedis.hvals(key);
         } finally {
-            sjp.returnResource(jedis);
+            //sjp.returnResource(jedis);
+            sjp.returnResourceObject(jedis);
         }
     }
 
-    public String get(String key) {
+    public static String get(String key) {
         ShardedJedis jedis = getRedis();
         try {
             return jedis.get(key);
         } finally {
-            sjp.returnResource(jedis);
+            //sjp.returnResource(jedis);
+            sjp.returnResourceObject(jedis);
         }
 
     }
 
-    public void setLifeCycle(String key, int time) {
+    public static void setLifeCycle(String key, int time) {
         ShardedJedis jedis = getRedis();
         Long expire = jedis.expire(key, time);
         logger.warn("Redis设置key过期时间是否已经存在【expire:" + expire + "】");
